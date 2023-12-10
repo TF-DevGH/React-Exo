3d38b90575f238ad2796a2f6d7e47f18d5f0ce60
Thomas Fantou <thomasfantoupro@gmail.com>
2023-12-10 08:12:01 +0100

    first commmit (yarn dégagé)

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import Banner from './Banner'
+import Cart from './Cart'
+
+function App() {
+	return (
+		<div>
+			<Banner />
+			<Cart />
+		</div>
+	)
+}
+
+export default App
+/*
+import Banner from './Banner'
+import Cart from './Cart'
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo"/>
+				<h1>Bonjour !</h1>
+				<a
+					className="App-link"
+					href="https://react.js.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+		<div>
+			<Banner />
+			<Cart />
+		</div>
+	)
+}
+
+export default App
+
+*/
